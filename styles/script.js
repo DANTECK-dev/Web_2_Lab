20abb873221a31a513cf1150413714c7948589ce
Александр Немеров <nemerov_a@mail.ru>
2022-11-10 07:48:13 +0800

    6LABA dONE1

--- styles/script.js
+++ styles/script.js
@@ -4,4 +4,9 @@ window.onload = function () {
         document.body.classList.add('loaded');
         document.body.classList.remove('loaded_hiding');
     }, 500);
+    window.setInterval(function(){
+        var now = new Date();
+        var clock = document.getElementById("clock");
+        clock.innerHTML = now.toLocaleTimeString();
+    }, 1000);
 }
