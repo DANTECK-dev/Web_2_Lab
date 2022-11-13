76f856ad7256e6d0b29a2885ffcb004362a89565
Александр Немеров <nemerov_a@mail.ru>
2022-11-14 00:14:39 +0800

    hdgdfdsfhjksdfds fsdf a

--- Bootstrap Indiv/assets/js/script.js
+++ Bootstrap Indiv/assets/js/script.js
@@ -7,10 +7,22 @@ window.onload = function () {
             document.getElementsByClassName("panel-body").item(0).style.background = '#f9f9f9'
             document.getElementsByClassName("panel-body").item(1).style.background = '#f9f9f9'
             document.getElementsByClassName("panel-body").item(2).style.background = '#f9f9f9'
-            document.getElementsByClassName("n").item(0).style.color = '#222'
+            let n = document.getElementsByClassName("width")
+            for (let i = 0; i < n.length; i++) {
+                n.item(i).style.color = '#111'
+                n.item(i).style.background = '#fdfdfd'
+            }
+            let mark = document.getElementsByClassName("mark")
+            for (let i = 0; i < mark.length; i++) {
+                mark.item(i).style.color = '#af5508'
+            }
             let a = document.getElementsByTagName("p")
             for (let i = 0; i < a.length; i++) {
                 a.item(i).style.color = '#222'
+            }
+            let table = document.getElementsByTagName("table")
+            for (let i = 0; i < table.length; i++) {
+                table.item(i).style.color = '#222'
             }
             document.getElementsByTagName("footer").item(0).style.borderColor = '#000'
         } else {
@@ -20,10 +32,22 @@ window.onload = function () {
             document.getElementsByClassName("panel-body").item(0).style.background = '#333'
             document.getElementsByClassName("panel-body").item(1).style.background = '#333'
             document.getElementsByClassName("panel-body").item(2).style.background = '#333'
-            document.getElementsByClassName("n").item(0).style.color = '#aaa'
+            let n = document.getElementsByClassName("width")
+            for (let i = 0; i < n.length; i++) {
+                n.item(i).style.color = '#aaa'
+                n.item(i).style.background = '#313031'
+            }
+            let mark = document.getElementsByClassName("mark")
+            for (let i = 0; i < mark.length; i++) {
+                mark.item(i).style.color = 'lightsalmon'
+            }
             let a = document.getElementsByTagName("p")
             for (let i = 0; i < a.length; i++) {
                 a.item(i).style.color = '#aaa'
+            }
+            let table = document.getElementsByTagName("table")
+            for (let i = 0; i < table.length; i++) {
+                table.item(i).style.color = '#aaa'
             }
             document.getElementsByTagName("footer").item(0).style.borderColor = '#f9f9f9'
         }
@@ -38,17 +62,36 @@ window.onload = function () {
     changeColor(day)
     window.setInterval(function () {
         now = new Date();
-        clock = document.getElementById("clock");
-        clock.innerHTML = "Текущее время: " + now.toLocaleTimeString().split(':')[0] + ":" + now.toLocaleTimeString().split(':')[1];
         hour = parseInt(now.toLocaleTimeString().split(':')[0])
         console.log(now.toLocaleTimeString())
         if ((hour > 7 && hour < 21) && day === true) {
-            console.log("смена на день "+ hour + " час " + day)
+            console.log("смена на день " + hour + " час " + day)
             day = changeColor(day)
         }
         if (!(hour > 7 && hour < 21) && day === false) {
-            console.log("смена на ночь "+ hour + " час " + day)
+            console.log("смена на ночь " + hour + " час " + day)
             day = changeColor(day)
         }
-    }, 1000);
+        if (!day) {
+            clock = document.getElementById("clock").innerHTML =
+                + String(now.getDate()).padStart(2, '0') + "/"
+                + String(now.getMonth() + 1).padStart(2, '0') + "/"
+                + now.getFullYear()
+                + " Текущее время: "
+                + now.toLocaleTimeString().split(':')[0]
+                + ":"
+                + now.toLocaleTimeString().split(':')[1]
+                + " Включена дневная тема";
+        } else {
+            clock = document.getElementById("clock").innerHTML =
+                + String(now.getDate()).padStart(2, '0') + "/"
+                + String(now.getMonth() + 1).padStart(2, '0') + "/"
+                + now.getFullYear()
+                + " Текущее время: "
+                + now.toLocaleTimeString().split(':')[0]
+                + ":"
+                + now.toLocaleTimeString().split(':')[1]
+                + " Включена ночная тема";
+        }
+    }, 1000 * 60);
 }
