37b633002b6f6d1fd6499f201bfb635269f32a79
Александр Немеров <nemerov_a@mail.ru>
2022-11-28 04:57:24 +0800

    7Done

--- Bootstrap Indiv/assets/js/script.js
+++ Bootstrap Indiv/assets/js/script.js
@@ -9,20 +9,62 @@ window.onload = function () {
         return Math.floor(Math.random() * (max - min)) + min;
     }
 
-    class Table{
-        col
-        data
-        constructor(col, data) {
-            this.col = col
-            this.data = data
+    class Table {
+        matrix
+
+        constructor(matrix) {
+            this.matrix = matrix
         }
-        GetTable(){
-            let str="<table><tbody>"
-            for(let row = 0; row < rows.length; row++){
-                str+=""
+
+        GetTable() {
+            let str = "<table><tbody>"
+            for (let row = 0; row < this.matrix.length; row++) {
+                str += `<tr>`
+                let rows = matrixx[row]
+                for (let col = 0; col < rows.length; col++) {
+                    if (this.matrix[row][col].indexOf("@#strong#@") >= 0){
+                        str += `<td><strong>` + this.matrix[row][col].replace("@#strong#@", "") + `</strong></td>`
+                        continue
+                    }
+
+                    if (this.matrix[row][col].indexOf("@#3#@") >= 0) {
+                        str += `<td align="center" colspan="3">` + this.matrix[row][col].replace("@#3#@", "") + `</td>`
+                        break
+                    } else if (this.matrix[row][col].indexOf("@#2#@") >= 0) {
+                        str += `<td align="center" colspan="2">` + this.matrix[row][col].replace("@#2#@", "") + `</td>`
+                        break
+                    }
+                    str +=`<td align="center">` + this.matrix[row][col] + `</td>`
+                }
+                str += `</tr>`
             }
+            return (str + `</tbody></table>`)
         }
     }
+
+    let matrixx = [
+        ["@#3#@Одноплатный мини-ПК Raspberry Pi"],
+        [" ", "@#strong#@Model A", "@#strong#@Model B"],
+        ["@#strong#@Цена", "$25", "$35"],
+        ["@#strong#@System-on-a-chip (SoC)", "@#2#@Broadcom BCM2835 (CPU + GPU)"],
+        ["@#strong#@CPU", "@#2#@700 МГц ARM11 (ядро ARM1176JZF-S), возможен разгон до 1 ГГц"],
+        ["@#strong#@GPU", "@#2#@Broadcom VideoCore IV"],
+        ["@#strong#@Стандарты", "@#2#@OpenGL ES 1.1/2.0, OpenVG 1.1, Open EGL, OpenMAX"],
+        ["@#strong#@Аппаратные кодеки", "@#2#@H.264 (1080p30, high-profile); MPEG-2 и VC-1 (лицензия продаётся отдельно)"],
+        ["@#strong#@Память (SDRAM, общая)", "@#2#@256 Мбайт", "512 Мбайт; 256 Мбайт (до 15.10.2012)"],
+        ["@#strong#@Порты USB 2.0", "1", "2"],
+        ["@#strong#@Видеовыход", "@#2#@1 x HDMI 1.3a (CEC), 1 x RCA (576i/480i, PAL-BGHID/M/N,NTSC, NTSC-J)"],
+        ["@#strong#@Аудиовыход", "@#2#@Гнездо 3,5 мм, HDMI", ""],
+        ["@#strong#@Карт-ридер", "@#2#@SD/MMC/SDIO"],
+        ["@#strong#@Сеть", "-", "Ethernet-порт RJ45 10/100 Мбит/с"],
+        ["@#strong#@Интерфейсы", "@#2#@20 x GPIO (SPI, I^(2)C, UART, TTL); MIPI CSI-2, MIPI DSI"],
+        ["@#strong#@Энергопотребление", "500 мА (2,5 Вт)", "700 мА (3,5 Вт)"],
+        ["@#strong#@Питание", "@#2#@5 В через порт micro-USB или GPIO"],
+        ["@#strong#@Размеры", "@#2#@85,6x56x21 мм"],
+        ["@#strong#@Масса", "@#2#@54 г"]
+    ];
+    let table = new Table(matrixx)
+    document.getElementById('table').innerHTML = table.GetTable()
 
     class Link {
         hyperlink
